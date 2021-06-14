7c6c2b950cbb1171e8fcb0d7e862fb1912a6edba
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-06-14 17:25:37 +0100

    Add script with basic functionality

--- batsave.sh
+++ batsave.sh
@@ -1,2 +1,45 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+command="$1"
+state="$( cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode )"
+
+if [[ $state == "1" ]];
+then
+	mode="ON"
+else
+	mode="OFF"
+fi
+
+if [[ $command == "on" ]];
+then
+	# Check if it is OFF
+	if [[ $mode == "OFF" ]];
+	then
+		# Turn on battery save mode
+		sudo sh -c "echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
+		echo "Battery save turned ON!"
+	else 
+		# No need to turn it on if it already is
+		echo "Battery save already turned ON!"
+	fi
+
+elif [[ $command == "off" ]];
+then
+	if [[ $mode == "ON" ]];
+	then
+		# Turn off battery save mode
+		sudo sh -c "echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
+		echo "Battery save turned OFF!"
+	else
+		echo "Battery save already turned OFF!"
+	fi
+
+else 
+	# Print usage and current status
+	echo "Usage:"
+	echo "batsave on|off|help"
+	echo "Current status:"
+	echo "Battery save is $mode"
+fi
+
 
